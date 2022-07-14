6d1abe78a2d8a73f4cdde714802dd5a7e9eb0839
Harshdeep Singh <56130865+coderhersh@users.noreply.github.com>
2022-07-14 18:06:05 +0530

    Update settings.py

--- LoginLogout/settings.py
+++ LoginLogout/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#5u%r=0i4yk&74a*mk#k5lga$8ff_#js7_9ny=fe0&^v)=ahpw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
