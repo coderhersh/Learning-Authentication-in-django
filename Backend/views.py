a5a5abb04e73ea6133ae85510fb7f005e2c035a2
coderhersh <deepharsh370@gmail.com>
2022-07-14 16:22:02 +0530

    Changes to be committed: 	modified:   Backend/views.py
     Changes not staged for commit:
    	modified:   templates/result.html

--- Backend/views.py
+++ Backend/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from Backend.models import User
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 def signin(request):
     if request.method == 'POST':
         entered_username = request.POST['user_name']
         entered_password = request.POST['password']
+
+        user = auth.authenticate(username=entered_username, password=entered_password)
+
+        if user is not None:
+            auth.login(request, user)
+        else:
+            messages.info(request, 'You have entered wrong credentials!!!')
+            redirect('/')
     return render(request, 'signin.html')
 
 def signup(request):
@@ -17,10 +26,20 @@ def signup(request):
         entered_password = request.POST['password']
         entered_confirmed_password = request.POST['confirm_password']
 
-        User(email=entered_email, user_id=entered_username, 
-        first_name=entered_firstname, last_name=entered_lastname,
-        password=entered_password).save()
+        if entered_password == entered_confirmed_password:
+            if User.objects.filter(email=entered_email).exists() == False:
+                if User.objects.filter(user_id=entered_username).exists() == False:        
+                    User(email=entered_email, user_id=entered_username, 
+                    first_name=entered_firstname, last_name=entered_lastname,
+                    password=entered_password).save()
+                else:
+                    messages.info(request, 'Username already exists!!!')
+            else:
+                messages.info(request, 'Email antered already exists!!')
+        else:
+            messages.info(request, 'Password is not matching!!')
 
+            
         print(entered_email, entered_firstname, entered_lastname, entered_email
         , entered_password)
 
